8de301e28bf1132d43810923b826b4fa0ab2f96a
Rakesh Singh <digitalorraads@gmail.com>
2026-03-05 12:27:24 +0530

    feat: Add and configure Axios instance with the backend base URL.

--- nexainfotech/src/Protected/axios.js
+++ nexainfotech/src/Protected/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://nexa-backend-xyul.onrender.com",
-  withCredentials: true, // 🔥 very important for session
+  withCredentials: true, // Required to send/receive session cookies across different domains
 });
 
 export default axiosInstance;
